27948446706727e33dcf4040ae1d85b6d881ff75
Swasthik R Prabhu <135737860+Swasthik-Prabhu@users.noreply.github.com>
2025-06-02 18:26:14 +0530

    Delete server directory

--- server/routes/student.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getStudentProfile } from "../controllers/studentController.js";
-import { protect } from "../middlewares/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/profile", protect, getStudentProfile);
-
-export default router;
